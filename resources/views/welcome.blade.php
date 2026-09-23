970ca20593ef2f44c1226010ac97835d2502c6f8
Kyuuin <helmitamami65@gmail.com>
2026-09-23 07:33:25 +0700

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hidupkan Jokowi</title>
+</head>
+<body>
+    
+    <h1>Website Html Pertama Di Library Laravel</h1>
+
+
+    <h1>Selamat datang di website pelajaran laravel</h1>
+    <hr>
+
+    <h2>Disini saya belajar html menggunakan library laravel dan semoga saya bisa mempelajari laravel</h2>
+
+
+
+
+</body>
+</html>
